9d980f45c7e39f26988ce682911c5dcfafb7406b
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-03-04 23:13:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='litepolis-router-template',                     # Change
-    version="v0.0.1",                                     # Change
-    description='The template module for LitePolis',      # Change
+    name='litepolis-router-example',
+    version="v0.0.1",
+    description='An example how to create API router module for LitePolis.',
     long_description=readme,
     long_description_content_type="text/markdown",
-    author='Your name',                                   # Change
+    author='David',
     # author_email='Optional',                            # Change
-    url='https://github.com/change-to-your-repo',         # Change
+    url='https://github.com/LitePolis-router-example',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=['fastapi'],
